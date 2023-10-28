dc83d771c95b9f16bf752fcc424370f13530d1db
Lars Weimar <Eisbaeeer@gmail.com>
2023-10-28 20:51:03 +0200

    Update main.cpp
    Working on code

--- src/main.cpp
+++ src/main.cpp
@@ -28,23 +28,6 @@ NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPi
 //NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod>* strip = NULL;
 NeoPixelAnimator animations(2); // only ever need 2 animations
 
-uint16_t lastPixel = 0; // track the eye position
-int8_t moveDir = 1; // track the direction of movement
-
-// uncomment one of the lines below to see the effects of
-// changing the ease function on the movement animation
-AnimEaseFunction moveEase =
-//      NeoEase::Linear;
-//      NeoEase::QuadraticInOut;
-//      NeoEase::CubicInOut;
-        NeoEase::QuarticInOut;
-//      NeoEase::QuinticInOut;
-//      NeoEase::SinusoidalInOut;
-//      NeoEase::ExponentialInOut;
-//      NeoEase::CircularInOut;
-
-// NeoPixelBus TEST ENDE
-
 // Update client
 WiFiClient updateclient;
 
@@ -80,88 +63,6 @@ DCF77 DCF = DCF77(LED_PIN,DCF_PIN,DCF_INTERRUPT);   // Interrupt DCF77
 // SUBROUTINES
 //*************************************************************************************
 
-// NEOPIXELBUS ANIMATION BEGIN
-void colorWipe(uint8_t R, uint8_t G, uint8_t B) {
-  RgbColor RGB(R,G,B);
-    for(uint8_t i=0; i<PixelCount; i++) { // For each pixel...
-      strip.SetPixelColor(i, RGB);
-      strip.Show();
-      delay(20);
-    }
-}
-
-void FadeAll(uint8_t darkenBy)
-{
-    RgbColor color;
-    for (uint16_t indexPixel = 0; indexPixel < 30; indexPixel++)
-    {
-        color = strip.GetPixelColor(indexPixel);
-        color.Darken(darkenBy);
-        strip.SetPixelColor(indexPixel, color);
-    }
-}
-
-void FadeAnimUpdate(const AnimationParam& param)
-{
-    if (param.state == AnimationState_Completed)
-    {
-        FadeAll(10);
-        animations.RestartAnimation(param.index);
-    }
-}
-
-void MoveAnimUpdate(const AnimationParam& param)
-{
-    // apply the movement animation curve
-    float progress = moveEase(param.progress);
-
-    // use the curved progress to calculate the pixel to effect
-    uint16_t nextPixel;
-    if (moveDir > 0)
-    {
-        nextPixel = progress * 30;
-    }
-    else
-    {
-        nextPixel = (1.0f - progress) * 30;
-    }
-
-    // if progress moves fast enough, we may move more than
-    // one pixel, so we update all between the calculated and
-    // the last
-    if (lastPixel != nextPixel)
-    {
-        for (uint16_t i = lastPixel + moveDir; i != nextPixel; i += moveDir)
-        {
-            strip.SetPixelColor(i, CylonEyeColor);
-        }
-    }
-    strip.SetPixelColor(nextPixel, CylonEyeColor);
-
-    lastPixel = nextPixel;
-
-    if (param.state == AnimationState_Completed)
-    {
-        // reverse direction of movement
-        moveDir *= -1;
-
-        // done, time to restart this position tracking animation/timer
-        animations.RestartAnimation(param.index);
-    }
-}
-
-void SetupAnimations()
-{
-    // fade all pixels providing a tail that is longer the faster
-    // the pixel moves.
-    animations.StartAnimation(0, 5, FadeAnimUpdate);
-
-    // take several seconds to move eye fron one side to the other
-    animations.StartAnimation(1, 2000, MoveAnimUpdate);
-}
-
-// NEOPIXELBUS END
-
 // function to crate HTML Colour
 void array_to_string(byte array[], unsigned int len, char buffer[])
 {
@@ -175,7 +76,6 @@ void array_to_string(byte array[], unsigned int len, char buffer[])
     buffer[len*2] = '\0';
 }
  
-
 void printDigits(int digits){
   // utility function for digital clock display: prints preceding colon and leading 0
   Serial.print(":");
@@ -218,11 +118,11 @@ void displayTime() {
   // Display time on LIXIE
   
   // LEDs aus bis auf Doppelpunkt
-  for(uint8_t i=0; i<12; i++) { // For each pixel...
+  for(uint8_t i=0; i<13; i++) { // For each pixel...
           RgbColor AUS(0,0,0);
           strip.SetPixelColor(i, AUS);
         }
-  for(uint8_t i=14; i<29; i++) { // For each pixel...
+  for(uint8_t i=14; i<30; i++) { // For each pixel...
           RgbColor AUS(0,0,0);
           strip.SetPixelColor(i, AUS);
         }
